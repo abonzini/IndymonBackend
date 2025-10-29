fea1bc332ba2e4aead7b190ea62490115e638b8c
Augusto Bonzini <augusto.bonzini@hitachirail.com>
2025-10-29 11:31:46 +0000

    Ongoing tournament can now be interrupted and stored. Team randomizer can now add a blank slot in last 3 moves but its obviously unlikely. Pokepaste now has the 1ev to avoid showdown warnings. Can re-roll a mon in a randomized team without needing to ban stuff. Cleaner tournament data entry and can be stopped and resumed, even if loaded from an indymon file

--- IndymonProgram/IndymonBackend/TournamentManager.cs
+++ IndymonProgram/IndymonBackend/TournamentManager.cs
@@ -4,10 +4,18 @@ namespace IndymonBackend
 {
     public class TournamentManager
     {
-        Tournament _ongoingTournament;
-        DataContainers _backEndData;
+        public Tournament OngoingTournament { get; set; }
+        DataContainers _backEndData = null;
         Random _rng = new Random();
         public TournamentManager(DataContainers backEndData)
+        {
+            _backEndData = backEndData;
+        }
+        public TournamentManager()
+        {
+
+        }
+        public void SetBackEndData(DataContainers backEndData)
         {
             _backEndData = backEndData;
         }
@@ -27,7 +35,7 @@ namespace IndymonBackend
                 {
                     case "elim":
                         validSelection = true;
-                        _ongoingTournament = new Tournament();
+                        OngoingTournament = new Tournament();
                         break;
                     default:
                         validSelection = false;
@@ -35,15 +43,15 @@ namespace IndymonBackend
                 }
             } while (!validSelection);
             Console.WriteLine("How many players will participate?");
-            _ongoingTournament.NPlayers = int.Parse(Console.ReadLine());
+            OngoingTournament.NPlayers = int.Parse(Console.ReadLine());
             Console.WriteLine("How many pokemon each team?");
-            _ongoingTournament.NMons = int.Parse(Console.ReadLine());
+            OngoingTournament.NMons = int.Parse(Console.ReadLine());
             // Finally, player selection
             List<TrainerData> trainers = _backEndData.TrainerData.Values.ToList();
             List<TrainerData> npcs = _backEndData.NpcData.Values.ToList();
             List<TrainerData> namedNpcs = _backEndData.NamedNpcData.Values.ToList();
             List<TrainerData> currentChosenTrainers = null;
-            int remainingPlayersNeeded = _ongoingTournament.NPlayers;
+            int remainingPlayersNeeded = OngoingTournament.NPlayers;
             bool randomizeFill = false;
             while (remainingPlayersNeeded > 0) // Will do addition loop until all players are selected
             {
@@ -99,7 +107,7 @@ namespace IndymonBackend
                 {
                     currentChosenTrainers.Remove(nextTrainer);
                     remainingPlayersNeeded--;
-                    _ongoingTournament.Participants.Add(nextTrainer);
+                    OngoingTournament.Participants.Add(nextTrainer.Name);
                     Console.WriteLine($"{nextTrainer.Name} added");
                 }
             }
@@ -111,20 +119,26 @@ namespace IndymonBackend
         {
             // First, shuffle the participants
             Random _rng = new Random();
-            int n = _ongoingTournament.Participants.Count;
+            int n = OngoingTournament.Participants.Count;
             while (n > 1) // Fischer yates
             {
                 n--;
                 int k = _rng.Next(n + 1);
-                (_ongoingTournament.Participants[k], _ongoingTournament.Participants[n]) = (_ongoingTournament.Participants[n], _ongoingTournament.Participants[k]); // Swap
+                (OngoingTournament.Participants[k], OngoingTournament.Participants[n]) = (OngoingTournament.Participants[n], OngoingTournament.Participants[k]); // Swap
             }
             // Ok not bad, next step is to update participant team sheet if needed, and generate the import pokepaste
             StringBuilder pokepasteBuilder = new StringBuilder();
-            foreach (TrainerData participant in _ongoingTournament.Participants)
+            foreach (string participantName in OngoingTournament.Participants)
             {
-                participant.DefineSets(_backEndData, _ongoingTournament.NMons); // Gets the team for everyone
+                // Try to find the participant
+                TrainerData participant;
+                if (_backEndData.TrainerData.ContainsKey(participantName)) participant = _backEndData.TrainerData[participantName];
+                else if (_backEndData.NpcData.ContainsKey(participantName)) participant = _backEndData.NpcData[participantName];
+                else if (_backEndData.NamedNpcData.ContainsKey(participantName)) participant = _backEndData.NamedNpcData[participantName];
+                else throw new Exception("Trainer not found!?");
+                participant.DefineSets(_backEndData, OngoingTournament.NMons); // Gets the team for everyone
                 pokepasteBuilder.AppendLine($"=== {participant.Name} ===");
-                pokepasteBuilder.AppendLine(participant.GetPokepaste(_backEndData, _ongoingTournament.NMons));
+                pokepasteBuilder.AppendLine(participant.GetPokepaste(_backEndData, OngoingTournament.NMons));
             }
             // Finally, ready to save the pokepaste
             string exportFile = Path.Combine(_backEndData.MasterDirectory, "importable-pokepaste.txt");
@@ -135,77 +149,100 @@ namespace IndymonBackend
         /// </summary>
         public void ExecuteTournament()
         {
-            _ongoingTournament.PlayTournament();
+            OngoingTournament.PlayTournament();
+        }
+    }
+    public class TournamentMatch()
+    {
+        public string player1 { get; set; } = "";
+        public string player2 { get; set; } = "";
+        public bool isBye { get; set; } = false;
+        public int score1 { get; set; } = 0;
+        public int score2 { get; set; } = 0;
+        public string winner { get; set; } = "";
+        public override string ToString()
+        {
+            return $"{player1} ({score1}-{score2}) {player2}";
         }
     }
     public class Tournament
     {
-        public int NPlayers = 0;
-        public int NMons = 3;
-        public List<TrainerData> Participants = new List<TrainerData>();
-        class TournamentMatch()
-        {
-            public string player1 = "";
-            public string player2 = "";
-            public bool isBye = false;
-            public int score1 = 0;
-            public int score2 = 0;
-            public string winner = "";
-            public override string ToString()
-            {
-                return $"\t{player1} {((player1 == winner) ? "(v)" : "")} {score1}-{score2} {((player2 == winner) ? "(v)" : "")} {player2}";
-            }
-        }
-        List<List<TournamentMatch>> _roundHistory = new List<List<TournamentMatch>>();
+        public int NPlayers { get; set; } = 0;
+        public int NMons { get; set; } = 3;
+        public List<string> Participants { get; set; } = new List<string>();
+        public List<List<TournamentMatch>> RoundHistory { get; set; } = null;
         /// <summary>
         /// Will play the tournament, organising the bracket and asking for results
         /// </summary>
         public void PlayTournament()
         {
-            int closestPowerOf2 = 1;
-            while (closestPowerOf2 < NPlayers) closestPowerOf2 *= 2; // Find the closest po2 above player name (will add byes)
-            int numberOfByes = closestPowerOf2 - NPlayers;
-            int byesBeginning = (numberOfByes / 2) + (numberOfByes % 2); // The beginning ones may have 1 more bye
-            int byesEnd = numberOfByes / 2; // The beginning ones may have 1 more bye
-            // Ok will start doing the matchups, first round
-            List<TournamentMatch> thisRound = new List<TournamentMatch>();
-            for (int i = 0; i < Participants.Count; i++)
+            if (RoundHistory == null) // Brand new tournament
             {
-                TournamentMatch thisMatch = new TournamentMatch();
-                thisMatch.player1 = Participants[i].Name;
-                if (i < byesBeginning)
+                RoundHistory = new List<List<TournamentMatch>>();
+                int closestPowerOf2 = 1;
+                while (closestPowerOf2 < NPlayers) closestPowerOf2 *= 2; // Find the closest po2 above player name (will add byes)
+                int numberOfByes = closestPowerOf2 - NPlayers;
+                int byesBeginning = (numberOfByes / 2) + (numberOfByes % 2); // The beginning ones may have 1 more bye
+                int byesEnd = numberOfByes / 2; // The beginning ones may have 1 more bye
+                // Ok will start doing the matchups, first round
+                List<TournamentMatch> thisRound = new List<TournamentMatch>();
+                for (int i = 0; i < Participants.Count; i++)
                 {
-                    thisMatch.isBye = true;
+                    TournamentMatch thisMatch = new TournamentMatch();
+                    thisMatch.player1 = Participants[i];
+                    if (i < byesBeginning)
+                    {
+                        thisMatch.isBye = true;
+                    }
+                    else if (i >= (Participants.Count - byesEnd))
+                    {
+                        thisMatch.isBye = true;
+                    }
+                    else // Not a bye in either side...
+                    {
+                        i++; // Get next player (opp)
+                        thisMatch.player2 = Participants[i];
+                        thisMatch.isBye = false;
+                    }
+                    thisRound.Add(thisMatch);
                 }
-                else if (i >= (Participants.Count - byesEnd))
-                {
-                    thisMatch.isBye = true;
-                }
-                else // Not a bye in either side...
-                {
-                    i++; // Get next player (opp)
-                    thisMatch.player2 = Participants[i].Name;
-                    thisMatch.isBye = false;
-                }
-                thisRound.Add(thisMatch);
+                RoundHistory.Add(thisRound);
             }
-            // Ok now the tournament begins
+            // This is the part that loads a tournament, visually it prints all matches and prompts user one by one
             bool finished = false;
             while (!finished)
             {
-                _roundHistory.Add(thisRound); // Add the round to match history
+                List<TournamentMatch> currentRound = RoundHistory.Last();
+                List<TournamentMatch> nextRound = new List<TournamentMatch>();
                 int playerProcessed = 0;
-                List<TournamentMatch> futureRound = new List<TournamentMatch>();
-                foreach (TournamentMatch match in thisRound)
+                Console.Clear();
+                Console.WriteLine("Insert scores for each match. 0 if you want it randomized, q to stop input temporarily");
+                int consoleEndPosition = currentRound.Count + 1; // Where the cursor will be after data entry
+                int maxStringLength = 0;
+                foreach (TournamentMatch match in currentRound) // Print all rounds first (so that they can be simulated if needed
                 {
+                    string matchString;
                     if (match.isBye)
                     {
-                        Console.WriteLine($"{match.player1} has a bye");
+                        matchString = $"{match.player1} gets a bye";
+                    }
+                    else
+                    {
+                        matchString = $"{match.player1} v {match.player2}";
+                    }
+                    maxStringLength = Math.Max(maxStringLength, matchString.Length);
+                    Console.WriteLine(matchString);
+                }
+                for (int i = 0; i < currentRound.Count; i++)
+                {
+                    TournamentMatch match = currentRound[i];
+                    if (match.isBye)
+                    {
                         match.winner = match.player1;
                     }
                     else
                     {
-                        Console.WriteLine($"Score for match between {match.player1} and {match.player2}? 0 if randomized");
+                        Console.SetCursorPosition(maxStringLength + 1, i + 1); // Put the cursor on the right, and starting from 1 (to avoid message string)
                         string scoreString = Console.ReadLine();
                         if (scoreString == "0")
                         {
@@ -221,6 +258,11 @@ namespace IndymonBackend
                                 match.score2 = _rng.Next(1, NMons + 1);
                             }
                         }
+                        else if (scoreString.ToLower() == "q") // If q, just stop here we'll need to restart after
+                        {
+                            finished = true;
+                            break; // Stops iteration
+                        }
                         else
                         {
                             string[] scores = scoreString.Split('-');
@@ -235,30 +277,30 @@ namespace IndymonBackend
                         {
                             match.winner = match.player2;
                         }
-                        Console.WriteLine($"\t{match.ToString()}");
                     }
                     if ((playerProcessed % 2) == 0) // Even players, first of the next match
                     {
                         TournamentMatch nextMatch = new TournamentMatch();
                         nextMatch.player1 = match.winner;
-                        futureRound.Add(nextMatch);
+                        nextRound.Add(nextMatch);
                     }
                     else // It's the player 2
                     {
-                        TournamentMatch nextMatch = futureRound.Last();
+                        TournamentMatch nextMatch = nextRound.Last();
                         nextMatch.player2 = match.winner;
                     }
                     playerProcessed++;
                 }
                 // Finally, now here's the next round, unless tournament was finished
-                if (thisRound.Count == 1) // was the finals...
+                if (finished || currentRound.Count == 1) // If all's finished (either manually or because this was the last round)
                 {
                     finished = true;
-                    Console.WriteLine($"\tTournament won by {thisRound.Last().winner}");
+                    Console.SetCursorPosition(0, consoleEndPosition); // Sets the console in the right place
+                    Console.WriteLine($"Tournament data entry done");
                 }
                 else
                 {
-                    thisRound = futureRound; // Otherwise move forward
+                    RoundHistory.Add(nextRound); // Adds the next round to the pile
                 }
             }
         }
